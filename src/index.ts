065b28e7858133ca3be35b523acc0305b43d4345
Alexandre C <beejayvolts@gmail.com>
2023-12-01 19:27:22 +0100

    1-2

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
-import f1p1 from "./1-1.js";
+import e1p1 from "./1-1.js";
+import e1p2 from "./1-2.js";
 
-console.log("1-1 answer:", f1p1());
+console.log("1-1 answer:", e1p1());
+console.log("1-2 answer:", e1p2());
