8971b7f9c0f090ea87135a2f70c2b3a32bac446c
Alexandre C <beejayvolts@gmail.com>
2023-12-02 11:33:50 +0100

    2-1; 2-2

---
+++ src/2-1.ts
@@ -0,0 +1,32 @@
+import fs from "fs";
+
+const LIMITS = {
+  red: 12,
+  green: 13,
+  blue: 14,
+};
+
+function isSetInvalid(set: string) {
+  return (
+    set.split(",").filter((pair) => {
+      const number = parseInt(pair);
+      const color = pair.split(" ")[2] as "red" | "green" | "blue";
+
+      return number > LIMITS[color];
+    }).length !== 0
+  );
+}
+function isGameValid(line: string) {
+  return line.split(";").filter(isSetInvalid).length === 0;
+}
+function sumGamesId(lines: string[]) {
+  return lines.reduce((prev, line, index) => {
+    if (!isGameValid(line.slice(line.indexOf(":") + 1))) return prev;
+    return prev + index + 1;
+  }, 0);
+}
+export default () => {
+  const data = fs.readFileSync("./assets/2.txt", "utf-8");
+
+  return sumGamesId(data.split("\r\n"));
+};
