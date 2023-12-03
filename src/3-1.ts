650d9450582a101272d8f9f579b7b05ab7da1b56
Alexandre C <beejayvolts@gmail.com>
2023-12-03 17:37:18 +0100

    3-1

---
+++ src/3-1.ts
@@ -0,0 +1,45 @@
+import fs from "fs";
+
+function isDigit(map: string[], x: number, y: number) {
+  if (!map[y]) return false;
+  return !isNaN(parseInt(map[y].charAt(x)));
+}
+function isDigitAdjacentToSymbol(map: string[], x: number, y: number) {
+  const numLen = parseInt(map[y].slice(x, x + 3)).toString().length;
+
+  for (let i = x - 1; i < x + numLen + 1; i++) {
+    for (let j = y - 1; j < y + 2; j++) {
+      if (
+        map[j]?.charAt(i) &&
+        map[j].charAt(i) !== "." &&
+        !isDigit(map, i, j)
+      ) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+function sumNumbersAdjacentToSymbols(map: string[]) {
+  return map.reduce((prev, line, y) => {
+    return (
+      prev +
+      line.split("").reduce((prev, _, x) => {
+        if (
+          isDigit(map, x, y) &&
+          !isDigit(map, x - 1, y) &&
+          isDigitAdjacentToSymbol(map, x, y)
+        ) {
+          return prev + parseInt(map[y].slice(x, x + 3));
+        }
+        return prev;
+      }, 0)
+    );
+  }, 0);
+}
+
+export default () => {
+  const data = fs.readFileSync("./assets/3.txt", "utf-8");
+
+  return sumNumbersAdjacentToSymbols(data.split("\r\n"));
+};
