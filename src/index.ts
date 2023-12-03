e4f2f2dee7d2c7f2e167e67996d7f1225d32c479
Alexandre C <beejayvolts@gmail.com>
2023-12-03 19:45:20 +0100

    3-2

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,12 @@ import e1p1 from "./1-1.js";
 import e1p2 from "./1-2.js";
 import e2p1 from "./2-1.js";
 import e2p2 from "./2-2.js";
+import e3p1 from "./3-1.js";
+import e3p2 from "./3-2.js";
 
 console.log("1-1 answer:", e1p1());
 console.log("1-2 answer:", e1p2());
 console.log("2-1 answer:", e2p1());
 console.log("2-2 answer:", e2p2());
+console.log("3-1 answer:", e3p1());
+console.log("3-2 answer:", e3p2());
