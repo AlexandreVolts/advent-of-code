0db28f492303138c344125e13fafc14f16fceb2a
Alexandre C <beejayvolts@gmail.com>
2023-12-04 18:21:27 +0100

    4-2

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,9 @@ import e2p1 from "./2-1.js";
 import e2p2 from "./2-2.js";
 import e3p1 from "./3-1.js";
 import e3p2 from "./3-2.js";
+import e4p1 from "./4-1.js";
+import e4p2 from "./4-2.js";
 
-console.log("1-1 answer:", e1p1());
-console.log("1-2 answer:", e1p2());
-console.log("2-1 answer:", e2p1());
-console.log("2-2 answer:", e2p2());
-console.log("3-1 answer:", e3p1());
-console.log("3-2 answer:", e3p2());
+[e1p1, e1p2, e2p1, e2p2, e3p1, e3p2, e4p1, e4p2].forEach((callback, index) => {
+  console.log(`${~~(index / 2) + 1}-${(index % 2) + 1} answer: ${callback()}`);
+});
