f23bc3f666ca6b81a4ac5acd829aedf3623a4d9d
Alexandre C <beejayvolts@gmail.com>
2023-12-04 18:21:27 +0100

    4-2

---
+++ src/4-2.ts
@@ -0,0 +1,40 @@
+import fs from "fs";
+
+function getCardPoints(line: string) {
+  const winning = line
+    .slice(line.indexOf(":") + 1, line.indexOf("|"))
+    .split(" ");
+  const numbers = line.slice(line.indexOf("|") + 1);
+
+  return winning.reduce((prev, num) => {
+    if (!num || numbers.indexOf(num.padStart(3, " ")) === -1) return prev;
+    return prev + 1;
+  }, 0);
+}
+function sumInstance(cards: number[], instance: number, memoizer: number[]) {
+  let output = 1;
+
+  if (memoizer[instance]) return memoizer[instance];
+  if (cards[instance] === undefined) return 0;
+  for (let i = 1; i <= cards[instance]; i++) {
+    output += sumInstance(cards, instance + i, memoizer);
+  }
+  memoizer[instance] = output;
+  return output;
+}
+function sumCards(cards: string[]) {
+  const memoizer: number[] = [];
+
+  return cards
+    .map(getCardPoints)
+    .reduce(
+      (prev, _, index, arr) => prev + sumInstance(arr, index, memoizer),
+      0
+    );
+}
+
+export default () => {
+  const data = fs.readFileSync("./assets/4.txt", "utf-8");
+
+  return sumCards(data.split("\r\n"));
+};
