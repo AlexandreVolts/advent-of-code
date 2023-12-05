0db006a43796990aeee12e78fd4f826e283ee867
Alexandre C <beejayvolts@gmail.com>
2023-12-05 16:21:00 +0100

    5-2

---
+++ src/5-2.ts
@@ -0,0 +1,41 @@
+import fs from "fs";
+import { ConverterMap, getConverterSystem } from "./5-helpers";
+
+function convertResultToSeed(result: number, system: ConverterMap[]) {
+  return system.reduce((prev, converterMap) => {
+    for (const converter of converterMap) {
+      const converted = prev - (converter[0] - converter[1]);
+      if (converted < converter[1] || converted >= converter[1] + converter[2])
+        continue;
+      return prev - (converter[0] - converter[1]);
+    }
+    return prev;
+  }, result);
+}
+function isResultIntoSeedsInterval(result: number, seeds: number[]) {
+  for (let i = seeds.length - 1; i >= 0; i -= 2) {
+    if (result >= seeds[i - 1] && result < seeds[i - 1] + seeds[i]) return true;
+  }
+  return false;
+}
+function getLowestLocation(lines: string[]) {
+  const seeds = lines[0]
+    .split(" ")
+    .slice(1)
+    .map((value) => parseInt(value));
+  const system = getConverterSystem(lines.slice(2)).reverse();
+  let output = 0;
+
+  for (
+    ;
+    !isResultIntoSeedsInterval(convertResultToSeed(output, system), seeds);
+    output++
+  );
+  return output;
+}
+
+export default () => {
+  const data = fs.readFileSync("./assets/5.txt", "utf-8");
+
+  return getLowestLocation(data.split("\r\n"));
+};
